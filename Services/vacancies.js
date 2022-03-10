83b34d9c8b9d8cf375b0ba971ba26b0ddf9c975a
Lakshani-Galwatta <85465588+Lakshani-Galwatta@users.noreply.github.com>
2022-03-10 14:33:26 +0530

    vacancies

---
+++ Services/vacancies.js
@@ -0,0 +1,115 @@
+// const Vacancy = require('../Model/vacancies');
+// const mysql= require ('serverless-mysql');
+const mysql = require('../dbconfig');
+
+module.exports = {
+
+async createVacancy (vacancy) {
+  let sql = "INSERT INTO vacancies SET ?";
+  let result = mysql.query(sql, vacancy, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+
+  if(result) {
+    return {
+      data: vacancy,
+      message: "Vacancy successfully created!"
+    };
+  }
+  return "Error creating new vacancy"
+},
+
+async acceptVacancy (vacancyId) {
+  let result = await mysql.query({
+    sql: 'UPDATE vacancies set VacancyStatus=1 where vacancyId = '+ mysql.escape(vacancyId),
+    timeout: 10000,
+    values: ['serverless']
+  })
+
+  if(result)  {
+  return result;
+  }
+    return "Error fetching vacanciess from db"
+},
+
+async getAllVacancies()  {
+  	
+  let result = await mysql.query({
+    sql: 'SELECT * FROM vacancies',
+    timeout: 10000,
+    values: ['serverless']
+  })
+
+  if(result)  {
+  
+    return result;
+
+  }
+    return "Error fetching vacancies from db"
+},
+  
+async getVacancyById(vacancyId)  {
+
+let result = await mysql.query({
+  sql: 'SELECT * FROM vacancies where vacancyId = '+ mysql.escape(vacancyId),
+  timeout: 10000,
+  values: ['serverless']
+})
+
+if(result)  { 
+return result;
+}
+  return "Error fetching vacancies from db"
+},
+
+
+
+async deleteVacancy(vacancyId)  { 	
+let result = await mysql.query({
+  sql: 'DELETE FROM vacancies where vacancyId = '+ mysql.escape(vacancyId),
+  timeout: 10000,
+  values: ['serverless']
+})
+
+if(result)  {
+return result;
+}
+  return "Error fetching vacancies from db"
+},
+
+async getPendingVacancies()  {
+ 	
+  let result = await mysql.query({
+    sql: 'SELECT * FROM vacancies where VacancyStatus=0',
+    timeout: 10000,
+    values: ['serverless']
+  })
+
+  if(result)  {
+  
+    return result;
+
+  }
+    return "Error fetching vacancies from db"
+},
+
+async getAcceptedVacancies()  {
+  	
+  let result = await mysql.query({
+    sql: 'SELECT * FROM vacancies where VacancyStatus=1',
+    timeout: 10000,
+    values: ['serverless']
+  })
+
+  if(result)  {
+  
+    return result;
+
+  }
+    return "Error fetching vacancies from db"
+}
+}
+
+
