f61cefa1b2ba9320db5728a3e3e492f55a131387
SashiniDawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2022-03-11 10:09:46 +0530

    image-album added

--- dbconfig.js
+++ dbconfig.js
@@ -1,21 +1,23 @@
 //var mysql = require('serverless-mysql')()
+require("dotenv").config();
 const mysql = require('serverless-mysql')()
- mysql.config({
-    host     :process.env.DB_HOST,
-    database :process.env.DB_DATABASE,
-    user     : process.env.DB_USER,
-    password :process.env.DB_PASS,
-    port     :process.env.DB_PORT,
+mysql.config({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
     timeout: 10000
-  })
+})
 
 
-mysql.connect((err) => {i
-  if (!err) {
-    console.log("Connected");
-  } else {
-    console.log("Connection Failed");
-  }
+mysql.connect((err) => {
+    i
+    if (!err) {
+        console.log("Connected");
+    } else {
+        console.log("Connection Failed");
+    }
 });
 
-module.exports =mysql;
+module.exports = mysql;
