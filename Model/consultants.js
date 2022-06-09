ad64dabc427798f5b3fd1ecae1edd46e7e331e56
NipuniNuwanthika <wmnipuninuwanthika@gmail.com>
2022-06-10 00:16:28 +0530

    model-consultants, consultation

---
+++ Model/consultants.js
@@ -0,0 +1,13 @@
+class consultant {
+    constructor(consultantId,consultantFName,consultantLName,universityName,post,email) {
+        this.consultantId=consultantId;
+        this.consultantFName = consultantFName;
+        this.consultantLName = consultantLName;
+        this.universityName=universityName;
+        this.post=post;
+        this.email=email;
+
+    }
+}
+
+module.exports = consultant;
